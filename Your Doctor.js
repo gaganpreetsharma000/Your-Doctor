4bdf93f2abd4b1da924566476eba0bade3dd5d91
G.P Sharma <101785462+gaganpreetsharma000@users.noreply.github.com>
2024-01-18 16:41:40 +0530

    Add files via upload

---
+++ Your Doctor.js
@@ -0,0 +1,17 @@
+let num = prompt("enter your height");
+if(num >= 7 && num <= 8){
+    console.log("As per your height, your weight should be between 90 and 120 ");
+}
+else if(num >=6  && num <=6.9){
+    console.log("As per your height, your weight should be between 75 and 90");
+}
+else if(num >= 5 && num <=5.9){
+    console.log("As per your height, your weight should be between 60 and 75");
+}
+else if(num >= 4 && num <=4.9){
+    console.log("As per your height, your weight should be between 45 and 60");
+}
+else {
+    console.log("Can't help you")
+}
+ 
